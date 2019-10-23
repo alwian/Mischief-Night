15647cb8cd754c129d775ccd7d19012797ab3445
Notloc <ColtonJamesCampbell@gmail.com>
2019-10-23 15:22:27 -0300

    Implemented some interfaces to allow the player to interact with world objects

---
+++ Mischief Night/Assets/Scripts/Interfaces/IGameObject.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * A helper interface. Allows us to skip casting interfaces to Monobehaviours
+ * when we need access to Unity functions or the transform.
+ */
+using UnityEngine;
+
+public interface IGameObject
+{
+    GameObject gameObject { get; }
+    Transform transform { get; }
+}
---
+++ Mischief Night/Assets/Scripts/MovableObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovableObject : MonoBehaviour, IPhysicsObject
+{
+    new Rigidbody rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public bool IsPickedUp { get; private set; }
+
+    public void Pickup(Transform newParent)
+    {
+        rigidbody.isKinematic = true;
+        IsPickedUp = true;
+
+        this.transform.parent = newParent;
+        this.transform.ResetLocal();
+    }
+    public void Drop()
+    {
+        rigidbody.isKinematic = false;
+        IsPickedUp = false;
+    }
+}
