1440758c1dd76704639b199c490306e2f7a55da2
Alex Anderson <alex@alwian.com>
2019-10-29 12:50:34 -0300

    Added lines for tracing AI path

---
+++ Mischief Night/Assets/Patrol.cs
@@ -0,0 +1,30 @@
+﻿// Patrol.cs
+using UnityEngine;
+
+
+public class Patrol : MonoBehaviour
+{
+
+    public Transform[] points;
+    readonly Color[] colors = new Color[5]
+    {
+        Color.red,
+        Color.yellow,
+        Color.green,
+        Color.blue,
+        Color.magenta
+    };
+
+    private void OnDrawGizmos()
+    {
+        if (points.Length >= 2) {
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                Gizmos.color = colors[i % colors.Length];
+                Gizmos.DrawLine(points[i].position, points[i + 1].position);
+            }
+        }
+    }
+        
+    
+}
