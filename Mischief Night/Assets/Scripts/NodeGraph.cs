da89c8dce75c321936a52fa9840d87fba89177b9
Notloc <ColtonJamesCampbell@gmail.com>
2019-11-20 17:08:39 -0400

    Began work on Level 3 and the maze's audio system

---
+++ Mischief Night/Assets/Scripts/NodeGraph.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeGraph : MonoBehaviour
+{
+    List<Node> nodes = new List<Node>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        ;
+    }
+}
