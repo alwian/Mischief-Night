66590d01e57975f23447db5ee8ff951b89d97cd8
Notloc <ColtonJamesCampbell@gmail.com>
2019-11-25 12:42:49 -0400

    Added some culling triggers to the highschools classrooms and changed the camera to use a deferred render due to amount of lights in the scene.

---
+++ Mischief Night/Assets/Scripts/CullingTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CullingTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject[] cullObjects;
+
+    private void EnableAll()
+    {
+        foreach (var obj in cullObjects)
+            obj.SetActive(true);
+    }
+    private void DisableAll()
+    {
+        foreach (var obj in cullObjects)
+            obj.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        DisableAll();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        EnableAll();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        DisableAll();
+    }
+}
