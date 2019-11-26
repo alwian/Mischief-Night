2ff051838047bb924a5991cdd8301cb946998e2c
Notloc <ColtonJamesCampbell@gmail.com>
2019-11-25 20:09:23 -0400

    Added the ability to die to the Player and reload the scene in response

--- Mischief Night/Assets/Scripts/GameManager.cs
+++ Mischief Night/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -24,6 +25,11 @@ public class GameManager : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
--- Mischief Night/Assets/Scripts/Player.cs
+++ Mischief Night/Assets/Scripts/Player.cs
@@ -2,7 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class Player : MonoBehaviour
 {
+    PlayerController controller;
 
+    private void Awake()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    public void Kill()
+    {
+        controller.enablePlayerControl = false;
+        GameManager.Instance.Reload();
+    }
 }
