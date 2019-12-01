1a3bb93988a08e31b99f3b74576ca2250ba1e998
AmandaJN <amanda.j_n@hotmail.com>
2019-12-01 16:15:44 -0400

    Audio Adding Part 1
    Imported Audio assets. Implemented Game Music Ambient Music sounds. Implemented dimenion change sound with fade to black screen

--- Mischief Night/Assets/Scripts/Audio/AbstractAudio.cs
+++ Mischief Night/Assets/Scripts/Audio/AbstractAudio.cs
@@ -1,9 +1,8 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class AbstractAudio : MonoBehaviour
 {
-    private AudioClip clip;
-
     public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
     {
         float startVolume = audioSource.volume;
@@ -11,31 +10,18 @@ public abstract class AbstractAudio : MonoBehaviour
         while (audioSource.volume > 0)
         {
             audioSource.volume -= startVolume * 0.1f;
-
             yield return null;
         }
-
         audioSource.Stop();
     }
 
-    protected abstract void Play(){
-        AudioSource audio = GetComponent<AudioSource>(); //creates new audio source
-        audio.clip = audioClip[0]; //finds clip from array of clips
-        audio.Play(); //plays track
-        Invoke("playNextTrack", audio.clip.length); //calls the play next track method when track finishes playing
+    protected abstract void Play();
+
+    protected void Stop(AudioSource source){
+        AudioSource audio = source.GetComponent<AudioSource>();
+        audio.Stop();
     }
 
-    protected abstract void Stop(){
-        AudioSource audio = GetComponent<AudioSource>();
-        audio.stop();
-    }
-
-    protected abstract void Next(){
-        AudioSource audio = GetComponent<AudioSource>(); //creates a new audio source
-        audio.Stop(); //stops other audio just in case
-        audio.clip = audioClip[1]; //finds next track in the array
-        audio.Play(); //plays track
-        Invoke("playMusic", audio.clip.length); //calls the play music track again when track finishes playing
-    }
+    protected abstract void Next();
 
 }
