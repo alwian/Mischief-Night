cbc14c05e2e572d283845841bf16f14af818b49d
Notloc <ColtonJamesCampbell@gmail.com>
2019-12-02 14:05:41 -0400

    Made the ritual stones spin

---
+++ Mischief Night/Assets/Scripts/Spin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField] float spinSpeed = 5;
+
+    private void Update()
+    {
+        this.transform.rotation = Quaternion.Euler(0f, spinSpeed * Time.deltaTime, 0f) * this.transform.rotation;
+    }
+}
